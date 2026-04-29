ee8d6277a0db0507f2fb9e9bfdf1818f0ac5d604
ameliadion <adion@smith.edu>
2026-04-28 22:55:59 -0400

    edited plot_progress and added more tests to test-year_pr_funct

--- R/plot_progress.R
+++ R/plot_progress.R
@@ -8,8 +8,10 @@
 #' @import lubridate
 #' @import dplyr
 #' @import ggplot2
+#' @import plotly
 #' @examples
 #' plot_progress(twohundred_fly)
+#' plot_progress(twohundred_back)
 
 plot_progress <- function(x){
   # clean function here
@@ -25,9 +27,13 @@ plot_progress <- function(x){
     dplyr::group_by(year, month) |>
     dplyr::slice_min(time_sec, n = 1) |>
     dplyr::ungroup()
-  ggplot2::ggplot(x_mutated, ggplot2::aes(x = interaction(month, year), y = time_sec, group = 1)) +
-    ggplot2::geom_line() +
+  ggplot2::ggplot(x_mutated, ggplot2::aes(x = interaction(month, year), y = time_sec, group = 1,
+                                          text = paste0("<b>Month and Year:</b> ", interaction(month, year), "<br>",
+                                                        "<b>Time (seconds):</b> ", time_sec, "<br>"))) +
+    ggplot2::geom_point() +
+    ggplot2::geom_smooth(method = "lm", se = FALSE) +
     ggplot2::labs(x = "Month and Year", y = "Best Time per Month (seconds)") +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
+  plotly::ggplotly(p = ggplot2::last_plot(), tooltip = "text")
 }
 
--- tests/testthat/test-year_pr_funct.R
+++ tests/testthat/test-year_pr_funct.R
@@ -2,6 +2,20 @@ test_that("year pr function works", {
   # test a simple case
   expect_equal(yearly_pr(twohundred_fly, 2026), "2:05.86")
 
-  # test against known value
+  # test that an error will occur with an input year not included in data
   expect_error(yearly_pr(twohundred_fly, 2006))
+
+  # tests that function can handle ties of fastest time
+  # (2021 has two equivalent times)
+  expect_equal(yearly_pr(twohundred_back, 2021), "2:08.82")
+
+  # tests that return is a character string of length 1
+  result <- yearly_pr(twohundred_fly, 2024)
+  expect_type(result, "character")
+  expect_length(result, 1)
+
+  # tests that is filters correctly by year
+  # (Dec 16, 2018 -> 2:27.35, Dec 16, 2022 -> 2:10.92)
+  expect_equal(yearly_pr(twohundred_fly, 2018), "2:27.35")
+
 })
